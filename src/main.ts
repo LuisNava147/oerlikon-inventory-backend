4d292f381c2e97d196164722ddbd43b3e790574b
LuisNava147 <luis.navas147@gmail.com>
2025-12-03 16:32:48 -0600

    Feature: añadimos seguridad y validaciones ValidationPipe para insertar datos correctos en la BD

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Borra campos que no estén en el DTO (limpieza)
+      forbidNonWhitelisted: true, //// Lanza error si mandan campos extra (seguridad estricta)
+      transform: true, // Convierte tipos automáticamente (ej: string a number en params)
+    })
+  )
   await app.listen(3005);
 }
 bootstrap();
