6dd9075b64f9fd679399363c0318573bc915bc57
LuisNava147 <luis.navas147@gmail.com>
2025-12-03 18:56:14 -0600

    Fix: employees.service. Create exceptions to employees.service

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException, InternalServerErrorException, ConflictException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import {v4 as uuid} from "uuid";
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './entities/employee.entity';
 import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class EmployeesService {
@@ -15,8 +16,12 @@ export class EmployeesService {
 {}  
   
   async create(createEmployeeDto: CreateEmployeeDto) {
-    const employees = this.employeeRepository.create(createEmployeeDto)
-    return this.employeeRepository.save(employees);
+    try{
+      const employees = this.employeeRepository.create(createEmployeeDto)
+      return await this.employeeRepository.save(employees);
+    }catch(error){
+      throw this.duplicateDBKeyError(error)
+    }
   }
 
   findAll() {
@@ -25,26 +30,48 @@ export class EmployeesService {
     
   }
 
-  findOne(id: string) {
-    const employee = this.employeeRepository.findOne({
+  async findOne(id: string) {
+    const employee = await this.employeeRepository.findOne({
       where:{
         employeeId: id
       }
     })
+    if(!employee)throw new NotFoundException("empleado no encontrado")
     return employee;
   }
 
   async update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
-    const employee = await this.employeeRepository.preload({
-      employeeId: id,
-      ...updateEmployeeDto
-    })
-    if(!employee)throw new NotFoundException("No se puede actualizar")
-    return await this.employeeRepository.save(employee);
+    try{
+      const employee = await this.employeeRepository.preload({
+        employeeId: id,
+        ...updateEmployeeDto
+      })
+      if(!employee)throw new NotFoundException("No se puede actualizar")
+      return await this.employeeRepository.save(employee);
+    }catch(error){
+      throw this.handleDBError(error)
+    }
     
   }
 
   async remove(id: string) {
-    return await this.employeeRepository.delete(id);
+    await this.employeeRepository.delete(id);
+    return{
+      message: "Empleado Eliminado"
+    }
+  }
+
+  private handleDBError(error:any):never{
+    if(error.code == '23505'){
+      throw new BadRequestException("Bad Request")
+    }
+    throw new InternalServerErrorException("Error interno al actualizar el empleado")
+  }
+
+  private duplicateDBKeyError(error:any):never{
+    if(error.code == '23505'){
+      throw new ConflictException("Uno de estos o los dos valores ya está registrado: 'Correo'y/o'numero")
+    }
+    throw new InternalServerErrorException("Error interno al crear el empleado")
   }
 }
