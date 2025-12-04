65d3020cb9131ab214820b470657a93ac26653b4
LuisNava147 <luis.navas147@gmail.com>
2025-12-03 19:17:15 -0600

    Feature: Creacion de tabla Locations en BD, Entity & Dto

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,12 @@
-export class CreateLocationDto {}
+import { IsString, IsNotEmpty, IsOptional, IsNumber,MaxLength, MinLength } from 'class-validator';
+
+export class CreateLocationDto {
+    @IsString()
+    @MaxLength(30)
+    locationName: string
+
+    @IsString()
+    @MaxLength(200)
+    locationAddress: string
+
+}
--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,14 @@
-export class Location {}
+import {Entity, Column, PrimaryGeneratedColumn }from "typeorm";
+import { ManyToOne } from "typeorm";
+import { JoinColumn } from "typeorm";
+import { OneToOne } from "typeorm";
+
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn('increment')
+    locationId: number
+    @Column("text",{unique:true})
+    locationName: string
+    @Column("text",{nullable:true})
+    locationAddress: string
+}
