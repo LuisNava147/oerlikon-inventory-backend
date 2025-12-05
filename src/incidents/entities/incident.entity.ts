3bb83180191db887644ef1acc7e8646f7404aea7
LuisNava147 <luis.navas147@gmail.com>
2025-12-04 23:28:10 -0600

    Feature: incident entity

--- src/incidents/entities/incident.entity.ts
+++ src/incidents/entities/incident.entity.ts
@@ -1,1 +1,28 @@
-export class Incident {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Incident {
+    @PrimaryGeneratedColumn("uuid")
+    incidentId: string
+    @Column("text")
+    reportNumber: string
+    @Column({
+        type: 'date',
+        default: () => 
+        'CURRENT_DATE'
+    })
+    incidentDateOpening:  Date
+    @Column("text",{default: 'PENDIENTE'})
+    status: string
+    @Column("text")
+    incidentDescription: string
+    @Column("text",{nullable:true})
+    incidentNote: string
+    @Column({
+        type: 'date',
+        default: () => 
+        'CURRENT_DATE'
+    })
+    incidentDateClose:  Date
+
+}
