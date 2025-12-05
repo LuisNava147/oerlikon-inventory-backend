f5bef788dbf8f7ad3a525d8eea9dc61e3981dd6e
LuisNava147 <luis.navas147@gmail.com>
2025-12-04 23:24:05 -0600

    Feature: Creacion de Entities para Providers & TicketIncident & Incidents

--- src/providers/entities/provider.entity.ts
+++ src/providers/entities/provider.entity.ts
@@ -1,1 +1,15 @@
-export class Provider {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Provider {
+    @PrimaryGeneratedColumn("uuid")
+    providerId: string
+    @Column("text")
+    providerName: string
+    @Column("text",{unique:true, nullable: true})
+    providerEmail: string 
+    @Column("text",{unique:true, nullable:true})
+    providerPhoneNumber: string
+    @Column("text")
+    providerContactName: string    
+}
--- src/ticket-incidents/entities/ticket-incident.entity.ts
+++ src/ticket-incidents/entities/ticket-incident.entity.ts
@@ -1,1 +1,26 @@
-export class TicketIncident {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class TicketIncident {
+    @PrimaryGeneratedColumn("uuid")
+    ticketIncidentId: string
+    @Column("text")
+    ticketName: string
+    @Column("text")
+    ticketLink: string
+    @Column({
+        type: 'date',
+        default: ()=>
+        'CURRENT_DATE'
+    })
+    ticketDateOpening:  Date
+    @Column({
+        type: 'date',
+        default: () => 
+        'CURRENT_DATE'
+    })
+    ticketDateClose: Date
+    @Column("text",{default: 'PENDIENTE'})
+    status: string
+
+}
