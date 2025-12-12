873143525c5ce372677d9e357f69c385f2b0167c
LuisNava147 <luis.navas147@gmail.com>
2025-12-11 23:40:17 -0600

    Feature: Devices Entity & Devices module (creacion de tabla)

--- src/devices/entities/device.entity.ts
+++ src/devices/entities/device.entity.ts
@@ -1,1 +1,30 @@
-export class Device {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Device {
+@PrimaryGeneratedColumn("uuid")
+deviceId: string
+@Column("text",{unique:true, nullable:true})
+deviceHostName: string
+@Column("text",{unique:true, nullable:true})
+deviceAssetNumber: string
+@Column("text",{default:"Stock"})
+deviceStatus: string
+@Column("text")
+deviceType: string
+@Column("text")
+deviceModel: string
+@Column("text")
+deviceBrand: string
+@Column("text",{nullable:true})
+devicePassword:string
+@Column("text",{nullable:true})
+devicePin: string
+@Column("text",{nullable:true})
+ipAddress: string
+@Column("text",{nullable:true})
+sapName: string
+@Column("text",{nullable:true})
+deviceMAC: string
+
+}
