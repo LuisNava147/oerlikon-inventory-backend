891d13cb7c8ed70c2035fb029ce863b2487ad8dc
LuisNava147 <luis.navas147@gmail.com>
2025-12-11 23:24:37 -0600

    Feature: Providers, Ticket Incidents module(creacion de tabla en BD)

--- src/ticket-incidents/ticket-incidents.module.ts
+++ src/ticket-incidents/ticket-incidents.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TicketIncidentsService } from './ticket-incidents.service';
 import { TicketIncidentsController } from './ticket-incidents.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TicketIncident } from './entities/ticket-incident.entity';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([TicketIncident])
+  ],
   controllers: [TicketIncidentsController],
   providers: [TicketIncidentsService],
 })
