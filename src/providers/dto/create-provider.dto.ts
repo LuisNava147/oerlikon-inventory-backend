99a0126b3266977655cefaf32ce08047d6de5e3b
LuisNava147 <luis.navas147@gmail.com>
2025-12-18 04:05:03 -0600

    Feature: Create Entity for Providers

--- src/providers/dto/create-provider.dto.ts
+++ src/providers/dto/create-provider.dto.ts
@@ -1,1 +1,27 @@
-export class CreateProviderDto {}
+import { Location } from 'src/locations/entities/location.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsOptional, IsNumber,MaxLength, MinLength, IsInt, IsUUID } from 'class-validator';
+
+export class CreateProviderDto {
+@IsString()
+@MaxLength(80)
+@IsNotEmpty()
+providerName: string
+
+@IsString()
+@IsOptional()
+providerEmail: string
+
+@IsString()
+@IsOptional()
+@MaxLength(16)
+providerPhoneNumber: string
+
+@IsString()
+@MaxLength(180)
+providerContactName: string   
+
+@IsString()
+location: Location
+
+}
