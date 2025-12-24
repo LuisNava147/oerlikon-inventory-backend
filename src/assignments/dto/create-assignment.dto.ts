53c8eb9d94fcc0de8f9ed62fe648c56b153e762e
LuisNava147 <luis.navas147@gmail.com>
2025-12-24 10:10:12 -0600

    Feature: Create Assignment DTO & create pdf-template.ts

--- src/assignments/dto/create-assignment.dto.ts
+++ src/assignments/dto/create-assignment.dto.ts
@@ -1,5 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsOptional, IsNumber,MaxLength, MinLength, IsInt, IsUUID } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, IsNumber,MaxLength, MinLength, IsInt, IsUUID, IsDateString } from 'class-validator';
+import { Device } from 'src/devices/entities/device.entity';
+import { Employee } from 'src/employees/entities/employee.entity';
 
 export class CreateAssignmentDto {
+    @IsDateString()
+    @IsNotEmpty()
+    @ApiProperty({name:"YYYY-MM-DD"})
+    assignmentDate: Date
+
+    @IsDateString()
+    @IsOptional()
+    @ApiProperty({name:"YYYY-MM-DD"})
+    assignmentReturnDate?: Date
+
+    @IsString()
+    assigmentStatus: string
+
+    @IsString()
+    @IsOptional()
+    responsivaUrl?: string;
+
+    @IsUUID()
+    employee: Employee
+
+    @IsUUID()
+    device: Device
+
 }
