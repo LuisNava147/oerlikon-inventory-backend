2942c946342d4d84ec2957c62ccd9049180722fe
LuisNava147 <luis.navas147@gmail.com>
2025-12-29 09:00:35 -0600

    Feature: Auth/User module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Employee } from 'src/employees/entities/employee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Employee])],
   controllers: [AuthController],
   providers: [AuthService],
 })
