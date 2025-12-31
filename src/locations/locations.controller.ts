b832860e5d2ccfec6e35945082f6147490358309
LuisNava147 <luis.navas147@gmail.com>
2025-12-31 00:45:48 -0600

    Feature: Add Auth decorator to location.controller

--- src/locations/locations.controller.ts
+++ src/locations/locations.controller.ts
@@ -2,32 +2,41 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { LocationsService } from './locations.service';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { ApiAuth } from 'src/auth/decorators/api.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
+@ApiAuth()
 @Controller('locations')
 export class LocationsController {
   constructor(private readonly locationsService: LocationsService) {}
 
+  @Auth(ROLES.ADMIN)
   @Post()
   create(@Body() createLocationDto: CreateLocationDto) {
     return this.locationsService.create(createLocationDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Get()
   findAll() {
     return this.locationsService.findAll();
   }
 
+  @Auth(ROLES.ADMIN)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.locationsService.findOne(+id);
   }
 
+  @Auth(ROLES.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateLocationDto: UpdateLocationDto) {
     //console.log(updateLocationDto) verificar que si se actualice 
     return this.locationsService.update(+id, updateLocationDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.locationsService.remove(+id);
