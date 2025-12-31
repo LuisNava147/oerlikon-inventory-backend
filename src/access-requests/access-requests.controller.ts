2c700b8e1d25546ad2016b729dacfc6b79639c6c
LuisNava147 <luis.navas147@gmail.com>
2025-12-31 00:56:14 -0600

    Feature: Add Auth Decorator to access-request.controller

--- src/access-requests/access-requests.controller.ts
+++ src/access-requests/access-requests.controller.ts
@@ -3,26 +3,34 @@ import { AccessRequestsService } from './access-requests.service';
 import { CreateAccessRequestDto } from './dto/create-access-request.dto';
 import { UpdateAccessRequestDto } from './dto/update-access-request.dto';
 import type { Response } from 'express';
+import { ApiAuth } from 'src/auth/decorators/api.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
+@ApiAuth()
 @Controller('access-requests')
 export class AccessRequestsController {
   constructor(private readonly accessRequestsService: AccessRequestsService) {}
 
+  @Auth(ROLES.ADMIN)
   @Post()
   create(@Body() createAccessRequestDto: CreateAccessRequestDto) {
     return this.accessRequestsService.create(createAccessRequestDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Get()
   findAll() {
     return this.accessRequestsService.findAll();
   }
 
+  @Auth(ROLES.ADMIN)
   @Get(':id')
   findOne(@Param('id',ParseUUIDPipe) id: string) {
     return this.accessRequestsService.findOne(id);
   }
 
+  @Auth(ROLES.ADMIN)
   @Get(':id/pdf')
   async downloadPdf(@Param('id',ParseUUIDPipe) id: string,
   @Res() res : Response){
@@ -40,11 +48,13 @@ export class AccessRequestsController {
     res.send(pdfBuffer.buffer);
   }
 
+  @Auth(ROLES.ADMIN)
   @Patch(':id')
   update(@Param('id',ParseUUIDPipe) id: string, @Body() updateAccessRequestDto: UpdateAccessRequestDto) {
     return this.accessRequestsService.update(id, updateAccessRequestDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Delete(':id')
   remove(@Param('id',ParseUUIDPipe) id: string) {
     return this.accessRequestsService.remove(id);
