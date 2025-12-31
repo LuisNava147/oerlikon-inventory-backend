671d2a04c30cddc92e71dcef916050532cd4b798
LuisNava147 <luis.navas147@gmail.com>
2025-12-31 00:29:41 -0600

    Feature: add Auth to Department controller

--- src/deparments/deparments.controller.ts
+++ src/deparments/deparments.controller.ts
@@ -3,31 +3,40 @@ import { DeparmentsService } from './deparments.service';
 import { CreateDeparmentDto } from './dto/create-deparment.dto';
 import { UpdateDeparmentDto } from './dto/update-deparment.dto';
 import { ParseUUIDPipe } from '@nestjs/common';
+import { ApiAuth } from 'src/auth/decorators/api.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
+@ApiAuth()
 @Controller('departments')
 export class DeparmentsController {
   constructor(private readonly deparmentsService: DeparmentsService) {}
 
+  @Auth(ROLES.ADMIN)
   @Post()
   create(@Body() createDeparmentDto: CreateDeparmentDto) {
     return this.deparmentsService.create(createDeparmentDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Get()
   findAll() {
     return this.deparmentsService.findAll();
   }
 
+  @Auth(ROLES.ADMIN)
   @Get(':id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.deparmentsService.findOne(id);
   }
 
+  @Auth(ROLES.ADMIN)
   @Patch(':id')
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateDeparmentDto: UpdateDeparmentDto) {
     return this.deparmentsService.update(id, updateDeparmentDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.deparmentsService.remove(id);
