fbaa72e90732795a2bd78ec9a2e27cc4efbb800e
LuisNava147 <luis.navas147@gmail.com>
2025-12-31 09:35:33 -0600

    Feature: add new CORS to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser'; // Necesario para tu Auth
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Para la documentación
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+  /*const app = await NestFactory.create(AppModule,{
+    cors:{
+      origin: process.env.allowedOrigin,
+      credentials: true
+    }
+    
+     app.use(cookieParser());
+
+    const config = new DocumentBuilder()
+    .setTitle('Oerlikon Inventory API')
+    .setDescription('Api for oerlikon inventory management')
+    .setVersion('0.9')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+    */
 
   app.useGlobalPipes(
     new ValidationPipe({
