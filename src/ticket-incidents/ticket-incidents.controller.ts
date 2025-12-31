8ce23ec2079e6715c1f22dbddf031eb87cf92d76
LuisNava147 <luis.navas147@gmail.com>
2025-12-31 00:51:31 -0600

    Feature: Add Auth Decorator to ticket-incident.controller

--- src/ticket-incidents/ticket-incidents.controller.ts
+++ src/ticket-incidents/ticket-incidents.controller.ts
@@ -2,31 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from
 import { TicketIncidentsService } from './ticket-incidents.service';
 import { CreateTicketIncidentDto } from './dto/create-ticket-incident.dto';
 import { UpdateTicketIncidentDto } from './dto/update-ticket-incident.dto';
+import { ApiAuth } from 'src/auth/decorators/api.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
+@ApiAuth()
 @Controller('ticket-incidents')
 export class TicketIncidentsController {
   constructor(private readonly ticketIncidentsService: TicketIncidentsService) {}
 
+  @Auth(ROLES.ADMIN)
   @Post()
   create(@Body() createTicketIncidentDto: CreateTicketIncidentDto) {
     return this.ticketIncidentsService.create(createTicketIncidentDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Get()
   findAll() {
     return this.ticketIncidentsService.findAll();
   }
 
+  @Auth(ROLES.ADMIN)
   @Get(':id')
   findOne(@Param('id',ParseUUIDPipe) id: string) {
     return this.ticketIncidentsService.findOne(id);
   }
 
+  @Auth(ROLES.ADMIN)
   @Patch(':id')
   update(@Param('id',ParseUUIDPipe) id: string, @Body() updateTicketIncidentDto: UpdateTicketIncidentDto) {
     return this.ticketIncidentsService.update(id, updateTicketIncidentDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Delete(':id')
   remove(@Param('id',ParseUUIDPipe) id: string) {
     return this.ticketIncidentsService.remove(id);
