fd1fc1f2e396743b0e709f043c2fcab8c0a4e368
LuisNava147 <luis.navas147@gmail.com>
2025-12-31 00:54:29 -0600

    Feature: add Auth Decorator to assignment.controller

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -4,26 +4,34 @@ import { CreateAssignmentDto } from './dto/create-assignment.dto';
 import { UpdateAssignmentDto } from './dto/update-assignment.dto';
 import type { Response } from 'express';
 import { Employee } from 'src/employees/entities/employee.entity';
+import { ApiAuth } from 'src/auth/decorators/api.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
+@ApiAuth()
 @Controller('assignments')
 export class AssignmentsController {
   constructor(private readonly assignmentsService: AssignmentsService) {}
 
+  @Auth(ROLES.ADMIN)
   @Post()
   create(@Body() createAssignmentDto: CreateAssignmentDto) {
     return this.assignmentsService.create(createAssignmentDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Get()
   findAll() {
     return this.assignmentsService.findAll();
   }
 
+  @Auth(ROLES.ADMIN)
   @Get(':id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.assignmentsService.findOne(id);
   }
 
+  @Auth(ROLES.ADMIN)
   @Get(':id/pdf')
   async downloadPdf(@Param('id',ParseUUIDPipe) id: string, 
   @Res() res: Response,
@@ -41,11 +49,13 @@ export class AssignmentsController {
     res.send(pdfBuffer.buffer);
   }
 
+  @Auth(ROLES.ADMIN)
   @Patch(':id')
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateAssignmentDto: UpdateAssignmentDto) {
     return this.assignmentsService.update(id, updateAssignmentDto);
   }
 
+  @Auth(ROLES.ADMIN)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.assignmentsService.remove(id);
