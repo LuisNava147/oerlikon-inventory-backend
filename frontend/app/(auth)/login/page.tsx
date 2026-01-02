d548a50c6207dae8d0779b2fe57f9fd20298b17e
LuisNava147 <luis.navas147@gmail.com>
2026-01-02 12:10:43 -0600

    Feature REACT: Login page

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,4 +1,18 @@
+import { Button, Input } from "@heroui/react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function LoginPage(){
-    return "login";
+    return (
+        <div className="bg-white px-100 px-20 py-2 rounded-md">
+            <Image src="/oe-logo.svg" alt="Logo de Oerlikon" className="my-6 bottom-10" width={200} height={0}/>
+            <p className="text-2xl my-4 text-black">Iniciar Sesión</p>
+            <div className="flex flex-col gap-3 my-5 items-center">
+                <Input label= "firtsname.lastname@oerlikon.com" type="email" isRequired={true} className="text-2xl"/>
+                <Input label= "Contraseña" type="password" isRequired={true} size="sm" />
+            </div>
+        <Button color="primary" variant="solid" className="text-xl" type="submit">Iniciar Sesion</Button>
+        <p className="my-6"><Link href="/signup" className="text-red-500 underline">Registrate</Link></p>
+        </div>
+    )
 }
