d0df69411470788540751e18ff0e481299ac6bc8
LuisNava147 <luis.navas147@gmail.com>
2026-02-18 02:48:22 -0600

    Fix REACT: Login Page (add more styles and fix loading)

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,11 +1,12 @@
 'use client'
-import { Button, Input } from "@heroui/react";
+import { Button, Card, CardBody, CardHeader, Input } from "@heroui/react";
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { API_URL } from "@/constants";
 import { useFormStatus } from "react-dom";
+import { TriangleAlert } from "lucide-react";
 
 function SubmitButton(){
     const {pending} = useFormStatus()
@@ -19,14 +20,20 @@ function SubmitButton(){
 
 export default function LoginPage(){
     const [submitting, setSubmitting] = useState(false)
+    const [error, setError] = useState("");
     const router = useRouter()
-    const handleSubmit = async (e:any) => {
+
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
         setSubmitting(true)
         e.preventDefault()
-        const formData = new FormData(e.target);
-        let authData : any = {}
-        authData.userEmail = formData.get("userEmail")
-        authData.userPassword = formData.get("userPassword")
+        setError("")
+        const formData = new FormData(e.currentTarget);
+
+        const authData = {
+            userEmail: formData.get("userEmail"),
+            userPassword: formData.get("userPassword")
+        }
+
         try{
             const response = await fetch(`${API_URL}/auth/login`,{
                 method: "POST",
@@ -36,24 +43,50 @@ export default function LoginPage(){
                 body: JSON.stringify(authData),
                 credentials: 'include'
             });
-            if(response.status == 201) router.push('/dashboard');
-            setSubmitting(false)    
+            if(response.status == 201){
+                router.push('/dashboard')
+            }else{
+                setSubmitting(false)
+                setError("Credenciales incorrectas o Error de servidor")
+            }
 
         }catch(e){
             setSubmitting(false)
+            setError("Error de conexión")
         }
         return;
     }
     return (
-        <form className="bg-white px-100 px-20 py-8 rounded-md" onSubmit={handleSubmit}>
-            <Image src="/oe-logo.svg" alt="Logo de Oerlikon" className="my-6 bottom-10" width={200} height={0}/>
-            <p className="text-2xl my-4 text-black">Iniciar Sesión</p>
-            <div className="flex flex-col gap-3 my-5 items-center">
-                <Input label= "Correo" name="userEmail" placeholder= "firstname.lastname@oerlikon.com" type="email" isRequired={true} className="text-2xl"/>
-                <Input label= "Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
-            </div>
-            <Button color="primary" variant="solid" className="text-xl" type="submit" disabled={submitting}>{submitting ? "Enviando..." : "Iniciar Sesión"}</Button>
-        
-        </form>
+        <div className="flex h-screen w-full items-center justify-center">
+        <Card className="w-[420px] bg-white border border-slate-200 shadow-2xl flex flex-col gap-2 p-4">
+            <CardHeader className="flex flex-col gap-3 items-center w-full pb-0">
+                <Image src="/oe-logo.svg" alt="Logo de Oerlikon" priority className="mb-6" width={200} height={50}/>
+            </CardHeader>
+                <CardBody className="px-6 py-2 overflow-hidden">
+                <h1 className="text-2xl font-bold text-slate-800 text-center mb-6">Iniciar Sesión</h1>
+                <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+                <div className="flex flex-col gap-5 w-full mb-8">
+                    <Input label= "Correo" name="userEmail" placeholder= "firstname.lastname@oerlikon.com" type="email" isRequired variant="bordered" radius="sm" 
+                   classNames={{inputWrapper: "bg-slate-50 border-slate-200 group-data-[focus=true]:border-red-500",
+                   label: "text-slate-600 font-medium"}}/>
+                    <Input label= "Contraseña" name="userPassword" type="password" isRequired variant="bordered" radius="sm" 
+                    classNames={{inputWrapper: "bg-slate-50 border-slate-200 group-data-[focus=true]:border-red-500",
+                    label: "text-slate-600 font-medium"}}/>
+                    
+                    {error && (
+                        <div className="flex items-center justify-center gap-2
+                         bg-red-50 border border-red-100 text-red-600 px-3 
+                         py-2 rounded-lg w-full mb-4 text-xs font-medium animate-fadeIn">
+                            <TriangleAlert size={16} />
+                            <span>{error}</span>
+                        </div>
+                        )}
+                </div>
+                <Button color="primary" variant="solid" className="w-full font-semibold text-lg shadow-md" type="submit" isLoading={submitting} isDisabled={submitting}>{submitting ? "Validando..." : "Iniciar Sesión"}</Button>
+            
+            </form>
+                </CardBody>
+        </Card>
+        </div>
     )
 }
